cf86ab04c9258973185361b877c4f60f28be7b5c
DylanJacquin <dylanjacquin56@gmail.com>
2016-12-12 17:56:01 -0500

    Ajout des connexions BDD et traitement pour le module Course et Menu

--- app/src/main/java/com/uqac/frenchies/izicoloc/activities/menus_semaine/ActivityMenusSemaine.java
+++ app/src/main/java/com/uqac/frenchies/izicoloc/activities/menus_semaine/ActivityMenusSemaine.java
@@ -1,28 +1,44 @@
 package com.uqac.frenchies.izicoloc.activities.menus_semaine;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.uqac.frenchies.izicoloc.R;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ActivityMenusSemaine extends AppCompatActivity
 {
     ListView listeMenus;
     List<Menu> menus;
     MenuAdapter menuAdapter;
+    String codeColoc = "XER435";
 
     List<String> donnees;
 
@@ -48,6 +64,13 @@ public class ActivityMenusSemaine extends AppCompatActivity
         menus = new ArrayList<Menu>();
         initialiserJour();
         initialiserListe();
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        sauvegardeLocale();
     }
 
     private void initialiserListe()
@@ -138,26 +161,16 @@ public class ActivityMenusSemaine extends AppCompatActivity
     //Sauvegarde locale de tous les menus
     private void saveData()
     {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.clear();
-        for(int i=0 ; i<menus.size(); i++)
-        {
-            editor.putString(jours.get(i) + "m", menus.get(i).getMidi()); //Forme "jour" + m (matin)
-            editor.putString(jours.get(i) + "s", menus.get(i).getSoir()); //Forme "jour" + s (soir)
-        }
-        editor.commit();
+        sauvegardeLocale();
+        if (internetConnection())
+            sauvegardeDistante();
     }
 
     private void loadData()
     {
-        for(int i=0 ; i<jours.size() ; i++)
-        {
-            if(sharedPreferences.contains(jours.get(i) + "m") && sharedPreferences.contains(jours.get(i) + "s"))
-                menus.add(new Menu(jours.get(i), sharedPreferences.getString(jours.get(i)+"m", new String()),
-                                                 sharedPreferences.getString(jours.get(i)+"s", new String())));
-            else
-                menus.add(new Menu(jours.get(i), "-", "-"));
-        }
+        chargementLocal();
+        if(internetConnection())
+            chargementDistant();
         menus.get(0).setJour("Aujourd'hui");
         menus.get(1).setJour("Demain");
         menuAdapter.notifyDataSetChanged();
@@ -166,7 +179,6 @@ public class ActivityMenusSemaine extends AppCompatActivity
     //Long clic sur un item de la liste
     class LongItemList implements AdapterView.OnItemLongClickListener
     {
-
         @Override
         public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id)
         {
@@ -223,5 +235,159 @@ public class ActivityMenusSemaine extends AppCompatActivity
                 }
             return true; //Vérification clic long
         }
+    }
+
+    private void chargementLocal()
+    {
+        for(int i=0 ; i<jours.size() ; i++)
+        {
+            if(sharedPreferences.contains(jours.get(i) + "m") && sharedPreferences.contains(jours.get(i) + "s"))
+                menus.add(new Menu(jours.get(i), sharedPreferences.getString(jours.get(i)+"m", new String()),
+                        sharedPreferences.getString(jours.get(i)+"s", new String())));
+            else
+                menus.add(new Menu(jours.get(i), "-", "-"));
+        }
+    }
+
+    private void chargementDistant()
+    {
+        String getUrl = "http://maelios.zapto.org/izicoloc/getAllMenu.php";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest postRequest = new StringRequest(Request.Method.POST, getUrl,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        String[] tmp = response.split("\"");
+                        if(tmp.length > 9) //Au moins un élément dans la liste
+                        {
+                            for (int i = 9 ; i<tmp.length ; i += 20)
+                                for (int j = 0 ; j<menus.size() ; j++)
+                                    if(tmp[i].equals(jours.get(j))) //Correspondance de jour
+                                    {
+                                        menus.get(j).setMidi(tmp[i + 4]);
+                                        menus.get(j).setSoir(tmp[i + 8]);
+                                    }
+                        }
+                        else
+                        {
+                            initBDD();
+                        }
+                        menuAdapter.notifyDataSetChanged();
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.toString());
+                        Toast.makeText(ActivityMenusSemaine.this,error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("code_coloc", codeColoc);
+
+                return params;
+            }
+        };
+        requestQueue.add(postRequest);
+    }
+
+    private void sauvegardeLocale()
+    {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        for(int i=0 ; i<menus.size(); i++)
+        {
+            editor.putString(jours.get(i) + "m", menus.get(i).getMidi()); //Forme "jour" + m (matin)
+            editor.putString(jours.get(i) + "s", menus.get(i).getSoir()); //Forme "jour" + s (soir)
+        }
+        editor.commit();
+    }
+
+    private void sauvegardeDistante()
+    {
+        if(internetConnection())
+        {
+            String setUrl = "http://maelios.zapto.org/izicoloc/updateMenu.php";
+            RequestQueue requestQueue;
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+            StringRequest request = new StringRequest(Request.Method.POST, setUrl, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(ActivityMenusSemaine.this, error.toString(), Toast.LENGTH_LONG).show();
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("lunch_menu", menus.get(jourActuel).getMidi());
+                    params.put("dinner_menu", menus.get(jourActuel).getSoir());
+                    params.put("day_menu", jours.get(jourActuel));
+                    params.put("code_coloc", codeColoc);
+                    return params;
+                }
+            };
+            requestQueue.add(request);
+        }
+    }
+
+    //Si l'appareil est connecté à internet
+    private boolean internetConnection()
+    {
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            return true;
+        }
+        else
+            return false;
+    }
+
+    private void initBDD()
+    {
+        for(int i=0 ; i<jours.size() ; i++)
+            initJourBDD(i);
+    }
+
+    private void initJourBDD(final int index)
+    {
+        String setUrl = "http://maelios.zapto.org/izicoloc/insertMenu.php";
+        RequestQueue requestQueue;
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest request = new StringRequest(Request.Method.POST, setUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ActivityMenusSemaine.this, error.toString(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("lunch_menu", "-");
+                params.put("dinner_menu", "-");
+                params.put("day_menu", jours.get(index));
+                params.put("code_coloc", codeColoc);
+                return params;
+            }
+        };
+        requestQueue.add(request);
     }
 }
